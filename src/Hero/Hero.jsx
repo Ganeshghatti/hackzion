93750b2e13d6469e6c175415faa22284c7e74bbf
Ganesh <ghattiganesh8@gmail.com>
2023-11-23 19:33:57 +0530

    navbar

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css"
 
 export default function Hero() {
   return (
-    <div>
-      hero
-    </div>
-  )
+    <section
+      id="hero"
+      className="flex flex-col items-center justify-center h-screen w-screen"
+      style={{padding:"0 10vw"}}
+    >
+      <p className="text-5xl font-bold md:2xl">AMC ENGINEERING COLLEGE</p>
+      <p className="text-sm font-thin md:text-xs">
+        Affiliated to VTU, Belagavi | Approved by AICTE | NAAC A+ and NBA
+        ACCREDITED, Bannerghatta Main Road, Bangalore-560083
+      </p>
+      <p className="text-sm font-bold">DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING</p>
+      <p className="text-sm font-bold py-4">Presents</p>
+      <h1 className="hero-title">HACKZION</h1>
+      <p className="hero-quote text-2xl md:text:xl">Think, Code, Innovate</p>
+      <p className="hero-subheading">A NATIONAL LEVEL HACKATHON</p>
+      <p className="text-base font-light"> 24 Hours Coding  -  19 & 20 December 2023  -  Deadline 10/12/2023</p>
+    </section>
+  );
 }
