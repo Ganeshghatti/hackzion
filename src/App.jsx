120c5a52eaf59cb60e2b4c6d5642db8ccd8f7ae7
Ganesh <ghattiganesh8@gmail.com>
2023-11-23 18:53:23 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+import Navbar from "./Navbar/Navbar";
+import Hero from "./Hero/Hero";
+import Teams from "./Teams/Teams";
+import Timeline from "./Timeline/Timeline";
+import Coordinator from "./Coordinator/Coordinator";
+import Footer from "./Footer/Footer";
+import tag from "./Assets/tag.png";
+import circle from "./Assets/circle.png";
+
+export default function App() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  const particleOptions = {
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: false,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: false,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 1000,
+        },
+        value: 40,
+      },
+      opacity: {
+        value: 0.3,
+      },
+      shape: {
+        type: "images",
+        image: [
+          { src: circle, height: 30, width: 30 },
+          { src: tag, height: 30, width: 30 },
+        ],
+      },
+      size: {
+        value: 20,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 4,
+          size_min: 0.2,
+          sync: false,
+        },
+      },
+    },
+    detectRetina: true,
+  };
+
+  return (
+    <div
+      style={{
+        zIndex: 0,
+      }}
+    >
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+      />
+      <div className="flex flex-col items-center">
+        <Navbar />
+        <Hero />
+        <Teams />
+        <Timeline />
+        <Coordinator />
+        <Footer />
+      </div>
+    </div>
+  );
+}
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import logo from "./HackZionLogo.png";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { FaXmark } from "react-icons/fa6";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const [menu, setmenu] = useState(false);
+  const menuf = () => {
+    setmenu(!menu);
+  };
+  return (
+    <nav
+      className="w-4/5 fixed top-6 flex justify-between px-10 items-center py-2 md:w-full md:top-0"
+      style={{ gap: "15vw" }}
+    >
+      {menu && (
+        <>
+          <div
+            className="fixed w-screen h-screen top-0 left-0 z-30"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.353)" }}
+            onClick={menuf}
+          />
+          <ul className="w-3/4 gap-8 fixed right-0 top-0 h-screen z-50 flex flex-col justify-center items-center bg-black nav-mobile-menu" >
+            <li>
+              <a href="#home" onClick={menuf}>
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="#teams" onClick={menuf}>
+                TEAMS
+              </a>
+            </li>
+            <li>
+              <a href="#rules" onClick={menuf}>
+                RULES
+              </a>
+            </li>
+            <li>
+              <a href="#co-ordinators" onClick={menuf}>
+                CO-ORDINATORS
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={menuf}>
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </>
+      )}
+      <img src={logo} alt="" className="w-16" />
+      <ul className="flex flex-1 z-50 justify-around md:hidden">
+        <li>
+          <a href="#home">HOME</a>
+        </li>
+        <li>
+          <a href="#teams">TEAMS</a>
+        </li>
+        <li>
+          <a href="#rules">RULES</a>
+        </li>
+        <li>
+          <a href="#co-ordinators">CO-ORDINATORS</a>
+        </li>
+        <li>
+          <a href="#contact">CONTACT</a>
+        </li>
+      </ul>
+      <FaBarsStaggered className={`hidden cursor-pointer md:block ${!menu ? 'block' : 'hidden'}`} onClick={menuf} />
+      <FaXmark className={`z-50  cursor-pointer fixed top-10 right-10 ${menu ? 'block' : 'hidden'}`} />
+    </nav>
+  );
+}
