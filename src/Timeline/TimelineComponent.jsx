53131a41978fcead77001d746e0f6c5e6cc729b4
Ganesh <ghattiganesh8@gmail.com>
2023-12-14 21:36:17 +0530

    timeline

--- src/Timeline/TimelineComponent.jsx
+++ src/Timeline/TimelineComponent.jsx
@@ -17,7 +17,19 @@ import CloseIcon from "@mui/icons-material/Close";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import Typography from "@mui/material/Typography";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import LocalCafeRoundedIcon from "@mui/icons-material/LocalCafeRounded";
+import BreakfastDiningRoundedIcon from "@mui/icons-material/BreakfastDiningRounded";
+import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+import FlagCircleIcon from "@mui/icons-material/FlagCircle";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DinnerDiningIcon from "@mui/icons-material/DinnerDining";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import SelfImprovementIcon from "@mui/icons-material/SelfImprovement";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import LyricsIcon from "@mui/icons-material/Lyrics";
+import { green } from "@mui/material/colors";
 
 export default function TimelineComponent() {
   return (
@@ -31,7 +43,7 @@ export default function TimelineComponent() {
             className="text-white"
             style={{ fontSize: "20px" }}
           >
-            27 Nov
+            19th Dec , 7:30AM
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector />
@@ -41,26 +53,8 @@ export default function TimelineComponent() {
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              Registration starts
-            </p>
-            <p className="text-sm md:text-xs">
-              You can register by clicking on the "Register Now" button given above
-            </p>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineConnector />
-
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              Registration starts at 9:00 AM
-            </p>
-            <p className="text-sm md:text-xs">
-              Follow our social media handles for latest updates.
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Reporting Start
             </p>
           </TimelineContent>
         </TimelineItem>
@@ -72,24 +66,22 @@ export default function TimelineComponent() {
             className="text-white"
             style={{ fontSize: "20px" }}
           >
-            13 Dec
+            19 Dec , 8:30AM
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector />
-            <TimelineDot color="primary" variant="outlined">
-              <CloseIcon />
+            <TimelineDot color="primary">
+              <BreakfastDiningRoundedIcon />
             </TimelineDot>
             <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              Registration ends
-            </p>
-            <p className="text-sm md:text-xs">
-              Last date to register for the event
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Breakfast
             </p>
           </TimelineContent>
         </TimelineItem>
+
         <TimelineItem>
           <TimelineOppositeContent
             sx={{ m: "auto 0" }}
@@ -97,22 +89,18 @@ export default function TimelineComponent() {
             className="text-white"
             style={{ fontSize: "20px" }}
           >
-            14 Dec
+            19 Dec , 10:00AM
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
             <TimelineDot color="secondary">
-              <FilterAltIcon />
+              <TipsAndUpdatesIcon />
             </TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              Uploading shortlisted teams
-            </p>
-            <p className="text-sm md:text-xs">
-              The screening committee will shortlist the teams, and the 30 best
-              ideas will be selected
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Inauguration and End of Reporting
             </p>
           </TimelineContent>
         </TimelineItem>
@@ -124,21 +112,111 @@ export default function TimelineComponent() {
             className="text-white"
             style={{ fontSize: "20px" }}
           >
-            15 Dec
+            19 Dec , 10:45AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <LocalCafeRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Tea Break
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            19 Dec, 11:15AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <FlagCircleIcon color="green" />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Hackathon Begins
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            19 Dec,
+            <br /> 1:00PM-2:00PM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <LunchDiningIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Lunch Break
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            19 Dec ,<br /> 4:30PM-5:00PM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <LocalCafeRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Tea Break
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            19 Dec , 5:00PM
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
             <TimelineDot color="secondary">
-              <CurrencyRupeeIcon />
+              <VisibilityIcon />
             </TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              Last day for Payements
-            </p>
-            <p className="text-sm md:text-xs">
-              Shortlisted Teams must pay 500Rs for registration
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Review 1
             </p>
           </TimelineContent>
         </TimelineItem>
@@ -150,18 +228,18 @@ export default function TimelineComponent() {
             className="text-white"
             style={{ fontSize: "20px" }}
           >
-            19 Dec
+            19 Dec ,<br /> 8:30PM-9:30PM
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
             <TimelineDot color="primary">
-              <NotStartedIcon />
+              <DinnerDiningIcon />
             </TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              Start of Hackathon
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Dinner
             </p>
           </TimelineContent>
         </TimelineItem>
@@ -173,20 +251,226 @@ export default function TimelineComponent() {
             className="text-white"
             style={{ fontSize: "20px" }}
           >
-            20 Dec
+            19 Dec , 11:00PM
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            <TimelineDot color="secondary">
+              <VisibilityIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Review 2
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 1:00AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
             <TimelineDot color="primary">
+              <FitnessCenterIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Zumba and Tea break
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 4:00AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <LocalCafeRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Tea Break
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 5:00AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            <TimelineDot color="secondary">
+              <VisibilityIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Review 3
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 6:00AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <SelfImprovementIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Yoga session
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 8:30AM-9:30AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary">
+              <BreakfastDiningRoundedIcon />
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Breakfast
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 5:00AM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            <TimelineDot color="secondary">
+              <RemoveRedEyeIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              End of Hackathon and <br />
+              Final Review
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 11:15AM-12:00PM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <LyricsIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Cultural Events{" "}
+            </p>
+          </TimelineContent>
+        </TimelineItem>
+
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec , 12:00PM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            <TimelineDot color="secondary">
               <EmojiEventsIcon />
             </TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "32px" }}>
-            <p variant="h6" className="text-2xl md:text-lg" component="span">
-              End of Hackathon
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Results
             </p>
-            <p className="text-sm md:text-xs">Declaration of Results</p>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            className="text-white"
+            style={{ fontSize: "20px" }}
+          >
+            20 Dec,
+            <br /> 1:00PM
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "primary.main" }} />
+            <TimelineDot color="primary">
+              <LunchDiningIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "32px" }}>
+            <p variant="h6" className="text-xl md:text-lg" component="span">
+              Lunch
+            </p>
           </TimelineContent>
         </TimelineItem>
       </Timeline>
